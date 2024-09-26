915722feeafad3a9ea94d1100a043a069208fd8e
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-09-26 18:19:27 +0500

    all routes added

---
+++ src/controllers/authController.js
@@ -0,0 +1,79 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();  // Загружаем переменные окружения
+
+exports.registerUser = async (req, res) => {
+    const { login, password } = req.body;
+
+    try {
+        let user = await User.findOne({ login });
+        if (user) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'User already exists'
+            });
+        }
+
+        user = new User({ login, password });
+        await user.save();
+
+        const payload = { user: { id: user.id } };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+        res.status(200).json({
+            status: 'success',
+            token,
+            message: 'User registered successfully'
+        });
+    } catch (err) {
+        console.error('Registration Error: ', err.message || err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Server error',
+            error: err.message || err
+        });
+    }
+};
+
+// User login
+exports.loginUser = async (req, res) => {
+    const { login, password } = req.body;
+
+    try {
+        const user = await User.findOne({ login });
+        if (!user) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid Credentials'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Invalid Credentials'
+            });
+        }
+
+        const payload = { user: { id: user.id } };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+        res.status(200).json({
+            status: 'success',
+            token,
+            name: user.name,
+            surname: user.surname,
+            message: 'User logged in successfully',
+            user: user,
+        });
+    } catch (err) {
+        console.error('Login Error: ', err.message || err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Server error',
+            error: err.message || err
+        });
+    }
+};
--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const cors = require('cors');
 const courseRoutes = require('./routes/courseRoutes')
 const invoiceRoutes = require('./routes/invoiceRoutes')
 const counterRoutes = require('./routes/counterRoutes');
-const startInvoiceCronJob = require('./cron/invoiceCron');
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use('/api/v1', courseRoutes)
 app.use('/api/v1', invoiceRoutes)
 app.use('/api/v1/counter', counterRoutes)
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/transactions', require('./routes/transRoutes'));
 
-startInvoiceCronJob();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
