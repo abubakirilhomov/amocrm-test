cd2ff27dc90b1fcdb971ffdfbc1513fe852cccd2
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-09-27 15:23:53 +0500

    asdjnjsaida

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const courseRoutes = require('./routes/courseRoutes')
 const invoiceRoutes = require('./routes/invoiceRoutes')
 const counterRoutes = require('./routes/counterRoutes');
-
+const checkCourseRoutes = require('./routes/checkCourseRoutes')
 dotenv.config();
 
 connectDB();
@@ -18,6 +18,7 @@ app.use('/api/v1', invoiceRoutes)
 app.use('/api/v1/counter', counterRoutes)
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/transactions', require('./routes/transRoutes'));
+app.use('/api/courses', checkCourseRoutes);
 
 
 const PORT = process.env.PORT || 5000;
