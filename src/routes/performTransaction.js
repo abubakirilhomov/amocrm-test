4c23e3927143aab5fe3bdadaf45b635b370e9a7f
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-09-28 13:49:50 +0500

    ads

--- src/routes/performTransaction.js
+++ src/routes/performTransaction.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const Invoices = require('../models/invoiceModel'); // Импортируйте модель курсов
+const Invoices = require('../models/invoiceModel');
 const router = express.Router();
 
-// Метод для завершения транзакции
 router.post('/perform-transaction', async (req, res) => {
     const { transaction_id, status } = req.body.params;
 
@@ -13,14 +12,12 @@ router.post('/perform-transaction', async (req, res) => {
 
         case 2:
             console.log(`Transaction ${transaction_id} is completed.`);
-                await completeTransaction(transaction_id);
+            await completeTransaction(transaction_id);
             break;
 
         case -1:
         case -2:
-            // Транзакция отменена
             console.log(`Transaction ${transaction_id} is canceled.`);
-            // Здесь обновите статус курса в вашей системе
             await cancelTransaction(transaction_id);
             break;
 
@@ -35,7 +32,6 @@ router.post('/perform-transaction', async (req, res) => {
             });
     }
 
-    // Успешный ответ
     res.json({
         jsonrpc: '2.0',
         id: req.body.id,
@@ -43,19 +39,15 @@ router.post('/perform-transaction', async (req, res) => {
     });
 });
 
-// Функция для обновления статуса завершенной транзакции
 async function completeTransaction(transactionId) {
-    // Обновите статус курса в базе данных
-    // Например, вы можете обновить курс, связанный с этой транзакцией
     const course = await Invoices.findOneAndUpdate(
-        { invoiceNumber: transactionId }, // Убедитесь, что у вас есть поле для связи
-        { status: 'completed' }, // Обновите статус
+        { invoiceNumber: transactionId },
+        { status: 'completed' },
         { new: true }
     );
     return course;
 }
 
-// Функция для отмены транзакции
 async function cancelTransaction(transactionId) {
     const course = await Invoices.findOneAndUpdate(
         { invoiceNumber: transactionId },
