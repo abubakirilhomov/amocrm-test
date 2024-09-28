a5e3a09c13567fcf42ccce512e173c980f199d32
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-09-28 14:17:24 +0500

    asddasdas

--- src/routes/checkPerformRoute.js
+++ src/routes/checkPerformRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Courses = require('../models/courseModel');
-const Order = require('../models/orderModel');
+const Order = require('../models/ordersModel');
 const router = express.Router();
 
 router.post('/check-perform-transaction', async (req, res) => {
