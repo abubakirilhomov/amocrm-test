609ae8fd0a059c68135dc20d01214614aca7edee
AsilbekKarimov <asilbekkarimov3234@gmail.com>
2024-10-05 18:17:28 +0500

    ads

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -248,7 +248,7 @@ const performTransaction = async (req, res) => {
 
         transaction.state = 2;
         transaction.perform_time = Date.now();
-        transaction.status = 'ОПЛАЧЕНО';
+        // transaction.status = 'ОПЛАЧЕНО';
         await transaction.save();
 
         res.json({
