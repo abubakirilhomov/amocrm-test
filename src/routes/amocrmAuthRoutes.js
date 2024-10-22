7931dd48de50d2f689cd54c1fceea8d1460e9f2a
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-10-22 14:32:29 +0500

    asddas

--- src/routes/amocrmAuthRoutes.js
+++ src/routes/amocrmAuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/amocrmAuthControllers');
 
 // Маршрут для обработки callback от AmoCRM
 router.get('/callback', authController.handleAmoCrmCallback);
